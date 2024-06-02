1fdc17172dd680810dd7f6758ce133e59179fa75
BonusreeDatta <bonusree.datta@brainstation-23.com>
2024-06-02 11:35:27 +0600

    timemodified cell has added in db and some edit has done at lib file

--- lib.php
+++ lib.php
@@ -1,15 +1,22 @@
 <?php
 
-function quizspa_add_instance($quizspa) {
+function quizspa_add_instance($quizspa, $mform = null) {
     global $DB;
 
     $quizspa->timecreated = time();
-    $quizspa->id = $DB->insert_record('quizspa', $quizspa);
+    $quizspa->timemodified = $quizspa->timecreated;
 
-    return $quizspa->id;
+    return $DB->insert_record('quizspa', $quizspa);
 }
 
-function quizspa_update_instance($quizspa) {
+/**
+ * Updates an instance of the quizspa.
+ *
+ * @param stdClass $quizspa
+ * @param quizspa_mod_form $mform
+ * @return bool
+ */
+function quizspa_update_instance($quizspa, $mform = null) {
     global $DB;
 
     $quizspa->timemodified = time();
@@ -18,16 +25,41 @@ function quizspa_update_instance($quizspa) {
     return $DB->update_record('quizspa', $quizspa);
 }
 
+/**
+ * Deletes an instance of the quizspa.
+ *
+ * @param int $id
+ * @return bool
+ */
 function quizspa_delete_instance($id) {
     global $DB;
 
-    if (!$quizspa = $DB->get_record('quizspa', ['id' => $id])) {
+    if (!$quizspa = $DB->get_record('quizspa_', array('id' => $id))) {
         return false;
     }
 
-    $DB->delete_records('quizspa', ['id' => $quizspa->id]);
+    // // Delete related questions and answers
+    // $DB->delete_records('quizspa_question', array('quiz_id' => $id));
+    // $DB->delete_records('quizspa_answer', array('que_id' => $id));
+
+    // Delete the quiz itself
+    $DB->delete_records('quizspa', array('id' => $id));
 
     return true;
 }
 
+/**
+ * Returns the list of supported features.
+ *
+ * @param string $feature FEATURE_xx constant for requested feature
+ * @return mixed True if feature is supported, null if unknown
+ */
+function quizspa_supports($feature) {
+    switch($feature) {
+        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
+        case FEATURE_MOD_INTRO:               return true;
+        case FEATURE_BACKUP_MOODLE2:          return true;
+        default:                              return null;
+    }
+}
 
--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_quizspa';
-$plugin->version = 2024053102; 
+$plugin->version = 2024060103; 
 $plugin->requires = 2022041900; 
 $plugin->release = 'v0.2'; 
